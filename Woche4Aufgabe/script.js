faf52a2e78f186026888124b17843d7bdc7ba6f9
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2020-11-06 13:10:48 +0100

    Respnosiv Design

--- Woche4Aufgabe/script.js
+++ Woche4Aufgabe/script.js
@@ -1,3 +1,16 @@
 "use strict";
 console.log("Hallo Welt");
 //# sourceMappingURL=script.js.map
+
+function a1(): void {
+    let x: string = "Alles";
+    console.log(x);
+    func1();
+    console.log("Logo!");
+}
+
+a1();
+
+function func1(): void {
+    console.log("Klar?");
+}

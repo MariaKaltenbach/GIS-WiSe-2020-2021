8c29dec7abebf0c571b53d042f41163b9ba958f8
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2020-12-04 10:22:06 +0100

    Canvas angepasst

--- Woche6Aufgabe/script.js
+++ Woche6Aufgabe/script.js
@@ -2,36 +2,36 @@
 //namespace um Daten aus der data.ts zu exportieren
 var Eisladen;
 (function (Eisladen) {
+    let path = window.location.pathname;
+    let page = path.split("/").pop();
     //mit Canvas zeichnen
     let canvas = document.getElementById("myEiscreme");
     let context = canvas.getContext("2d");
     context.lineWidth = 3;
     //Eiskugel zeichnen
-    function icecream(_colorString) {
+    function icecream(_colorString, _x, _y) {
         context.beginPath();
         context.fillStyle = _colorString;
         context.strokeStyle = _colorString;
-        context.arc(150, 130, 120, 0, 2 * Math.PI);
+        context.arc(150 + _x, 130 + _y, 120, 0, 2 * Math.PI);
         context.closePath();
         context.fill();
         context.stroke();
     }
-    icecream(localStorage.getItem("eiskugelFarbe")); //Ergebnis der Eiscreme wird ausgegeben
     // Waffel zeichnen
-    function cone(_colorString) {
+    function cone(_colorString, _x, _y) {
         context.beginPath();
         context.fillStyle = _colorString;
         context.strokeStyle = _colorString;
-        context.moveTo(50, 200);
-        context.lineTo(150, 600);
-        context.lineTo(250, 200);
+        context.moveTo(50 + _x, 200 + _y);
+        context.lineTo(150 + _x, 600 + _y);
+        context.lineTo(250 + _x, 200 + _y);
         context.closePath();
         context.fill();
         context.stroke();
     }
-    cone(localStorage.getItem("waffelFarbe")); //Ergenis der Eiscreme wird ausgegeben
     //streusel zeichnen
-    function sprinkles(_colorString) {
+    function sprinkles(_colorString, _x, _y) {
         context.beginPath();
         context.fillStyle = _colorString;
         context.strokeStyle = _colorString;
@@ -70,7 +70,14 @@ var Eisladen;
         context.closePath();
         context.stroke();
     }
-    sprinkles(localStorage.getItem("streuselFarbe")); //Ergenbis der eiscreme wird ausgegeben
+    if (page == "Ergebnis.html") {
+        icecream(localStorage.getItem("eiskugelFarbe"), 0, 0); //Ergebnis der Eiscreme wird ausgegeben
+        cone(localStorage.getItem("waffelFarbe"), 0, 0); //Ergenis der Eiscreme wird ausgegeben
+        sprinkles(localStorage.getItem("streuselFarbe"), 0, 0); //Ergenbis der eiscreme wird ausgegeben
+    }
+    if (page == "index.html") {
+        neuZeichnen();
+    }
     //funnction um die for-Schleifen zu verbinden, damit sich der code nicht so oft widerholt
     /*function setupSelection(_selectElement: HTMLSelectElement, _data: any[],) {
 
@@ -133,93 +140,23 @@ var Eisladen;
         localStorage.setItem("streuselFarbe", _e.target.value);
         neuZeichnen();
     }
-    localStorage.setItem("Streusel", Eisladen.streuselVariationen[0].farbe);
-    streusel.value = localStorage.getItem("Streusel");
-    localStorage.setItem("Eiskugel", Eisladen.eiskugelVariationen[0].farbe);
-    eiskugel.value = localStorage.getItem("Eiskugel");
-    localStorage.setItem("Waffel", Eisladen.waffelVariationen[0].farbe);
-    waffel.value = localStorage.getItem("Waffel");
+    if (localStorage.getItem("streuselFarbe") == null) {
+        localStorage.setItem("streuselFarbe", Eisladen.streuselVariationen[0].farbe);
+    }
+    streusel.value = localStorage.getItem("streuselFarbe");
+    if (localStorage.getItem("eiskugelFarbe") == null) {
+        localStorage.setItem("eiskugelFarbe", Eisladen.eiskugelVariationen[0].farbe);
+    }
+    eiskugel.value = localStorage.getItem("eiskugelFarbe");
+    if (localStorage.getItem("waffelFarbe") == null) {
+        localStorage.setItem("waffelFarbe", Eisladen.waffelVariationen[0].farbe);
+    }
+    waffel.value = localStorage.getItem("waffelFarbe");
     //Funktion damit die farbe sich ändert sobal man etwas auswählt
     function neuZeichnen() {
-        icecream(localStorage.getItem("eiskugelFarbe"));
-        sprinkles(localStorage.getItem("streuselFarbe"));
-        cone(localStorage.getItem("waffelFarbe"));
-    }
-    neuZeichnen();
-    //Bestätigen Button um auf die "Ansicht" (Eisladenseite) zu kommen
-    let button = document.getElementsByTagName("button");
-    button[0].addEventListener("click", bestätigen);
-    function bestätigen(_e) {
-        let parent = document.getElementById("fillThis");
-        parent.innerHTML = "";
+        icecream(localStorage.getItem("eiskugelFarbe"), 300, 100);
+        cone(localStorage.getItem("waffelFarbe"), 40, 100);
+        sprinkles(localStorage.getItem("streuselFarbe"), 0, 0);
     }
 })(Eisladen || (Eisladen = {}));
-let canvas = document.getElementById("myEiscreme1");
-let context = canvas.getContext("2d");
-context.lineWidth = 3;
-//Eiskugel für die Unterseite
-function icecreamUnterseite(_colorString) {
-    context.beginPath();
-    context.fillStyle = _colorString;
-    context.strokeStyle = _colorString;
-    context.arc(300, 350, 120, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
-    context.stroke();
-}
-icecreamUnterseite(localStorage.getItem("eiskugelFarbe"));
-//waffel für die Unterseite
-function coneUnterseite(_colorString) {
-    context.beginPath();
-    context.fillStyle = _colorString;
-    context.strokeStyle = _colorString;
-    context.moveTo(500, 200);
-    context.lineTo(600, 600);
-    context.lineTo(700, 200);
-    context.closePath();
-    context.fill();
-    context.stroke();
-}
-coneUnterseite(localStorage.getItem("waffelFarbe"));
-//streusel zeichnen
-function sprinklesUnterseite(_colorString) {
-    context.beginPath();
-    context.fillStyle = _colorString;
-    context.strokeStyle = _colorString;
-    context.moveTo(80, 140);
-    context.lineTo(100, 150);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(170, 140);
-    context.lineTo(150, 150);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(230, 100);
-    context.lineTo(210, 110);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(180, 60);
-    context.lineTo(190, 80);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(130, 100);
-    context.lineTo(100, 100);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(100, 60);
-    context.lineTo(130, 40);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(190, 160);
-    context.lineTo(210, 180);
-    context.closePath();
-    context.stroke();
-}
-sprinklesUnterseite(localStorage.get("streuselFarbe"));
 //# sourceMappingURL=script.js.map
--- Woche6Aufgabe/script.ts
+++ Woche6Aufgabe/script.ts
@@ -1,4 +1,3 @@
-
 //namespace um Daten aus der data.ts zu exportieren
 namespace Eisladen {
 
@@ -17,7 +16,8 @@ namespace Eisladen {
     export interface Streusel extends Lebensmittel { }
 
 
-
+    let path: string = window.location.pathname;
+    let page: string = path.split("/").pop();
 
     //mit Canvas zeichnen
     let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("myEiscreme");
@@ -25,45 +25,45 @@ namespace Eisladen {
 
     context.lineWidth = 3;
 
+
     //Eiskugel zeichnen
-    function icecream(_colorString: string): void {
+    function icecream(_colorString: string, _x: number, _y: number): void {
 
         context.beginPath();
         context.fillStyle = _colorString;
         context.strokeStyle = _colorString;
-        context.arc(150, 130, 120, 0, 2 * Math.PI);
+        context.arc(150 + _x, 130 + _y, 120, 0, 2 * Math.PI);
         context.closePath();
         context.fill();
         context.stroke();
 
     }
-    icecream(localStorage.getItem("eiskugelFarbe"));  //Ergebnis der Eiscreme wird ausgegeben
+
 
 
     // Waffel zeichnen
-    function cone(_colorString: string): void {
+    function cone(_colorString: string, _x: number, _y: number): void {
 
         context.beginPath();
         context.fillStyle = _colorString;
         context.strokeStyle = _colorString;
-        context.moveTo(50, 200);
-        context.lineTo(150, 600);
-        context.lineTo(250, 200);
+        context.moveTo(50 + _x, 200 + _y);
+        context.lineTo(150 + _x, 600 + _y);
+        context.lineTo(250 + _x, 200 + _y);
         context.closePath();
         context.fill();
         context.stroke();
 
     }
-    cone(localStorage.getItem("waffelFarbe"));  //Ergenis der Eiscreme wird ausgegeben
 
 
     //streusel zeichnen
-    function sprinkles(_colorString: string): void {
+    function sprinkles(_colorString: string, _x: number, _y: number): void {
         context.beginPath();
         context.fillStyle = _colorString;
         context.strokeStyle = _colorString;
-        context.moveTo(80, 140);
-        context.lineTo(100, 150);
+        context.moveTo(80 , 140);
+        context.lineTo(100 , 150);
         context.closePath();
         context.stroke();
 
@@ -103,8 +103,19 @@ namespace Eisladen {
         context.closePath();
         context.stroke();
     }
-    sprinkles(localStorage.getItem("streuselFarbe"));  //Ergenbis der eiscreme wird ausgegeben
-    
+
+    if (page == "Ergebnis.html") {
+
+        icecream(localStorage.getItem("eiskugelFarbe"), 0, 0);  //Ergebnis der Eiscreme wird ausgegeben
+        cone(localStorage.getItem("waffelFarbe"), 0, 0);  //Ergenis der Eiscreme wird ausgegeben
+        sprinkles(localStorage.getItem("streuselFarbe"), 0, 0);  //Ergenbis der eiscreme wird ausgegeben
+
+    }
+
+    if (page == "index.html") {
+
+        neuZeichnen();
+    }
 
 
 
@@ -188,123 +199,40 @@ namespace Eisladen {
     }
 
 
-    localStorage.setItem("Streusel", streuselVariationen[0].farbe);
-    streusel.value = localStorage.getItem("Streusel");
-    localStorage.setItem("Eiskugel", eiskugelVariationen[0].farbe);
-    eiskugel.value = localStorage.getItem("Eiskugel");
-    localStorage.setItem("Waffel", waffelVariationen[0].farbe);
-    waffel.value = localStorage.getItem("Waffel");
+    if (localStorage.getItem("streuselFarbe") == null) {
+
+        localStorage.setItem("streuselFarbe", streuselVariationen[0].farbe);
+
+    }
+    streusel.value = localStorage.getItem("streuselFarbe");
+
+
+    if (localStorage.getItem("eiskugelFarbe") == null) {
+
+        localStorage.setItem("eiskugelFarbe", eiskugelVariationen[0].farbe);
+
+    }
+    eiskugel.value = localStorage.getItem("eiskugelFarbe");
+
+
+    if (localStorage.getItem("waffelFarbe") == null) {
+
+        localStorage.setItem("waffelFarbe", waffelVariationen[0].farbe);
+
+    }
+    waffel.value = localStorage.getItem("waffelFarbe");
+
+
 
 
     //Funktion damit die farbe sich ändert sobal man etwas auswählt
     function neuZeichnen(): void {
 
-        icecream(localStorage.getItem("eiskugelFarbe"));
-        sprinkles(localStorage.getItem("streuselFarbe"));
-        cone(localStorage.getItem("waffelFarbe"));
+        icecream(localStorage.getItem("eiskugelFarbe"), 300, 100);  
+        cone(localStorage.getItem("waffelFarbe"), 40, 100);  
+        sprinkles(localStorage.getItem("streuselFarbe"), 0, 0); 
     }
-    neuZeichnen();
+   
 
-
-
-    //Bestätigen Button um auf die "Ansicht" (Eisladenseite) zu kommen
-    let button: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName("button");
-
-    button[0].addEventListener("click", bestätigen);
-
-    function bestätigen(_e: Event): void {
-        let parent: HTMLDivElement = <HTMLDivElement>document.getElementById("fillThis");
-        parent.innerHTML = "";
-    }
 
 }
-
-
-
-
-let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("myEiscreme1");
-let context: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
-
-context.lineWidth = 3;
-
-//Eiskugel für die Unterseite
-function icecreamUnterseite(_colorString: string): void {
-
-    context.beginPath();
-    context.fillStyle = _colorString;
-    context.strokeStyle = _colorString;
-    context.arc(300, 350, 120, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
-    context.stroke();
-
-}
-icecreamUnterseite(localStorage.getItem("eiskugelFarbe"));
-
-
-//waffel für die Unterseite
-function coneUnterseite(_colorString: string): void {
-
-    context.beginPath();
-    context.fillStyle = _colorString;
-    context.strokeStyle = _colorString;
-    context.moveTo(500, 200);
-    context.lineTo(600, 600);
-    context.lineTo(700, 200);
-    context.closePath();
-    context.fill();
-    context.stroke();
-
-}
-coneUnterseite(localStorage.getItem("waffelFarbe"));
-
-
-//streusel zeichnen
-function sprinklesUnterseite(_colorString: string): void {
-    context.beginPath();
-    context.fillStyle = _colorString;
-    context.strokeStyle = _colorString;
-    context.moveTo(80, 140);
-    context.lineTo(100, 150);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(170, 140);
-    context.lineTo(150, 150);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(230, 100);
-    context.lineTo(210, 110);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(180, 60);
-    context.lineTo(190, 80);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(130, 100);
-    context.lineTo(100, 100);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(100, 60);
-    context.lineTo(130, 40);
-    context.closePath();
-    context.stroke();
-
-    context.beginPath();
-    context.moveTo(190, 160);
-    context.lineTo(210, 180);
-    context.closePath();
-    context.stroke();
-}
-sprinklesUnterseite(localStorage.get("streuselFarbe"));
-
-

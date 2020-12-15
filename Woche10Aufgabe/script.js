45cd3011d294329945301b1d1ac013b4c9a0d964
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2020-12-15 21:11:20 +0100

    Aufgabe 3.1 und 3.2 angefangen

--- Woche10Aufgabe/script.js
+++ Woche10Aufgabe/script.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.P_3_1Server = void 0;
 const Http = require("http");
+const Url = require("url");
 var P_3_1Server;
 (function (P_3_1Server) {
+    let form;
     console.log("Starting server");
     let port = Number(process.env.PORT);
     if (!port)
@@ -20,7 +22,23 @@ var P_3_1Server;
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
+        let url = Url.parse(_request.url, true);
+        for (let key in url.query) {
+            _response.write(key + ":" + url.query[key]);
+        }
+        let jsonString = JSON.stringify(url.query);
+        _response.write(jsonString);
+        async function send(_event) {
+            let formData = new FormData(form);
+            let query = new URLSearchParams(formData);
+            let response = await fetch(url + "?" + query.toString());
+            let responseText = await response.text();
+            await fetch("index.html?" + MediaQueryList.toString());
+            alert(responseText);
+            console.log(query);
+        }
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=script.js.map
--- Woche10Aufgabe/script.ts
+++ Woche10Aufgabe/script.ts
@@ -1,6 +1,11 @@
 import * as Http from "http";
+import * as Url from "url";
+
 
 export namespace P_3_1Server {
+
+    let form: HTMLFormElement;
+
     console.log("Starting server");
     let port: number = Number(process.env.PORT);
     if (!port)
@@ -16,11 +21,41 @@ export namespace P_3_1Server {
     }
 
 
+
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
+
+
+        let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+        for (let key in url.query) {
+            _response.write(key + ":" + url.query[key]);
+        }
+        let jsonString: String = JSON.stringify(url.query);
+        _response.write(jsonString);
+
+
+        async function send(_event: Event): Promise<void> {
+            let formData: FormData = new FormData(form);
+            let query: URLSearchParams = new URLSearchParams(<undefined>formData);
+            let response: Response = await fetch(url + "?" + query.toString());
+            let responseText: String = await response.text();
+            await fetch("index.html?" + MediaQueryList.toString());
+            alert(responseText);
+            console.log(query);
+
+        }
+
+
     }
+
+
+
 }
+
+
+
